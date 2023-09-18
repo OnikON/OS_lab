680a81c5f1458992b0153ff1be807e4482889ca4
OnikON <OnikON@OnikON>
2023-09-18 17:20:57 +0700

    2 Lab

---
+++ L2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void print (char *line);
+
+void* TreadMain (void* parameters);
+
+int main (void) {
+    int ProgramStatus = 0;
+    pthread_t thread;
+    
+    ProgramStatus = pthread_create(&thread, NULL, TreadMain, NULL);
+    if ( ProgramStatus != 0) {
+        printf("I can't create a thread erro [ '%d' ]", ProgramStatus);
+    }
+
+    ProgramStatus = pthread_join(thread, NULL);
+    if ( ProgramStatus != 0) {
+        printf("I can't make pthread_join erro [ '%d' ]", ProgramStatus);
+    }
+
+    if (ProgramStatus == 0) {
+        print("1 thread");
+        pthread_exit(NULL);
+    }
+    return ProgramStatus;
+}
+
+void print (char *line) {
+    for (int i = 0; i < 10; i++) {
+        printf("%s : hi UwU\n", line);
+    }
+}
+
+void* TreadMain (void* parameters) {
+    print("2 thread");
+}
