f6d4ffd8c6919f6da7589e6244cd4812be5e27ff
OnikON <OnikON@OnikON>
2023-09-18 21:54:59 +0700

    L3

---
+++ L3/main.c
@@ -0,0 +1,76 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_THREADS 4
+
+void print(char **argv, int InedxS, int IndexE);
+void *TreadMain(void *parameters);
+void logick(int *mass, int argc);
+
+#define PARAMETRS struct parametrs
+struct parametrs {
+  int start_index;
+  int end_index;
+  char **argv;
+};
+
+int main(int argc, char **argv) {
+  int ProgramStatus = 0;
+  pthread_t thread[NUM_THREADS];
+  int logickmass[NUM_THREADS];
+  logick(logickmass, argc);
+  // (argc / 4) : (argc % 4) logick
+  PARAMETRS param[NUM_THREADS];
+  for (int n_thread = 0; n_thread < NUM_THREADS; n_thread++) {
+    
+    if (n_thread == 0) {
+      param[NUM_THREADS].start_index = 1;
+      param[NUM_THREADS].end_index = logickmass[n_thread];
+    } else {
+      param[NUM_THREADS].start_index = logickmass[n_thread - 1] + 1;
+      param[NUM_THREADS].end_index = logickmass[n_thread];
+    }
+
+    param[NUM_THREADS].argv = argv;
+
+    ProgramStatus = pthread_create(&thread[n_thread], NULL, TreadMain, &param[NUM_THREADS]);
+    if (ProgramStatus != 0) {
+      printf("I can't create a [%d]thread erro [ '%d' ]", n_thread,
+             ProgramStatus);
+    }
+    ProgramStatus = pthread_join(thread[n_thread], NULL);
+  }
+
+  if (ProgramStatus == 0) {
+    pthread_exit(NULL);
+  }
+  return ProgramStatus;
+}
+
+void print(char **argv, int InedxS, int IndexE) {
+  for (int i = InedxS; i < IndexE; i++) {
+    printf("[%d]%s\n", i, argv[i]);
+  }
+}
+
+void *TreadMain(void *parameters) {
+    PARAMETRS *param = (PARAMETRS *)parameters;
+    print (param->argv, param->start_index, param->end_index);
+}
+
+void logick(int *mass, int argc) {
+  argc--;
+  int withouе_trace = argc / NUM_THREADS;
+  int trace = argc % NUM_THREADS;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    if (trace > 0) {
+      mass[i] = (withouе_trace * (i + 1)) + 1;
+      trace--;
+    } else {
+      mass[i] = (withouе_trace * (i + 1));
+    }
+    #ifdef TEST
+    printf ("mass[%d] = %d \n", i, mass[i]);
+    #endif
+  }
+}
---
+++ L3/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define NUM_THREADS 4
+#define B
+
+#ifdef A
+int main (int argc, char** argv) {
+    printf("argc = %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("[%d]%s\n", i, argv[i]);
+    }
+    return 0;
+}
+#endif
+
+#ifdef B
+
+void logick (int *mass, int argc) {
+    argc--;
+    int withouе_trace = argc / NUM_THREADS;
+    int trace = argc % NUM_THREADS;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (trace > 0) {
+            mass[i] = (withouе_trace * (i + 1)) + 1;
+            trace--;
+        }
+        else {
+            mass[i] = (withouе_trace * (i + 1));
+        }
+    }
+}
+
+int main (int argc, char** argv) {
+    int mass[4];
+    logick(mass, argc);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        printf("%d\n", mass[i]);
+    }
+}
+#endif
