754e6d6e27433fec14ab05af66f24dd8a683320a
OnikON <onikon@DESKTOP-C0JVR1B>
2023-10-10 06:01:38 +0300

    LR1

--- LR1/threads/thread.c
+++ LR1/threads/thread.c
@@ -10,28 +10,25 @@
 #define handle_error_en(en, msg) \
                do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
-void *mythread(void *arg);
+void *mythread(void *arg);	
 
-#define B_task
-#define C_task
-
-#ifdef C_task
-	const int GLOBAL = 4;
+#ifdef CTASK
+	int GLOBAL = 4;
 #endif
 
 int main() {
-#ifdef A_task
+#ifdef ATASK
 	pthread_t tid;
 #endif
 
-#ifdef B_task
+#ifdef BTASK
 	pthread_t tid[5];
 #endif
 	int err;
 
 	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
 
-#ifdef A_task
+#ifdef ATASK
 	err = pthread_create(&tid, NULL, mythread, NULL);
 	if (err) {
 		handle_error_en(err, "pthread_create");
@@ -42,22 +39,22 @@ int main() {
 	}
 #endif
 
-#ifdef B_task
+#ifdef BTASK
 	for (int N_thred = 0; N_thred < 5; N_thred++) {
 		err = pthread_create(&tid[N_thred], NULL, mythread, NULL);
 		if (err) {
 	    	handle_error_en(err, "pthread_create");
 		}
-#ifdef C_task
-		/*int errV = pthread_join(tid[N_thred], NULL);
+#ifdef CTASK
+		int errV = pthread_join(tid[N_thred], NULL);
 		if (errV) {
 	    	handle_error_en(errV, "pthread_join");
-		}*/
-		printf("    %s : %ld\n", __func__, tid[N_thred]);
+		}
+		printf("    %s : %ld\n\n", __func__, tid[N_thred]);
 #endif
 	}
 #endif
-
+	sleep(20);
 	pthread_exit(NULL);
 
 }
@@ -67,11 +64,20 @@ void *mythread(void *arg) {
 	//for (int i = 0; i < 100; i++)
 	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
 
-#ifdef C_task
+#ifdef CTASK
 	printf("%s : %ld\n", __func__, pthread_self());
 	int a = 5;
 	const int b = 23;
-	printf("%s : int a = %p\tconst int b = %p\tglobal = %p\n", __func__, &a, &b, &GLOBAL);
+	printf("%s : pointers :\n\tint a = %p\n\tconst int b = %p\n\tglobal = %p\n", __func__, &a, &b, &GLOBAL);
 #endif
+
+#ifdef DTASK
+
+	a++;
+	GLOBAL++;
+	printf("%s : int a = %d\tglobal = %d\n", __func__, a, GLOBAL);
+
+#endif
+
 	return NULL;
 }
