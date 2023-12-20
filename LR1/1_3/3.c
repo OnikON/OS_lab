185aa9583996bc8eed0d888c560fbc96d9e3232f
root <root@MAINPC>
2023-12-21 01:48:14 +0700

    f

--- LR1/1_3/3.c
+++ LR1/1_3/3.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <errno.h>
 #include <stdlib.h>
@@ -10,6 +11,7 @@
 // Глобальный счетчик
 volatile int counter = 0;
 volatile int flag = 0;
+volatile char* LINE;
 
 #define handle_error_en(en, msg)                                               \
   do {                                                                         \
@@ -19,6 +21,7 @@ volatile int flag = 0;
   } while (0)
 
 void *thread_function(void *arg);
+void cleanup_function(void *arg);
 
 int main() {
     pthread_t thread;
@@ -32,7 +35,7 @@ int main() {
     }
 
     // Ждем некоторое время, чтобы дать потоку выполниться
-    sleep(5);
+    sleep(1);
 
     // Отмена потока с использованием pthread_cancel()
     printf("%d\n", __LINE__);
@@ -40,6 +43,7 @@ int main() {
     if (ProgramStatus) {
         handle_error_en(ProgramStatus, "pthread_cancel");
     }
+    
     //flag = 1;
     // Ожидание завершения потока
     printf("%d\n", __LINE__);
@@ -56,12 +60,32 @@ int main() {
 
 void *thread_function(void *arg) {
     //while (!flag){
-    while (1) {
+    char* line = malloc(12 * sizeof(char));
+
+    strcpy(line, "Hello World");
+
+    pthread_cleanup_push(cleanup_function, line);
+    printf("%p\n",line);
+
+    while (!flag) {
         // Увеличиваем счетчик
-        counter++;
-        //printf("  !");
+        //counter++;
+        printf("%s\n", line);
         //sleep(1);
     }
 
+    //free(line);
+    pthread_cleanup_pop(1);
     pthread_exit(NULL);
+}
+
+void cleanup_function(void *arg) {
+    char *message = (char *)arg;
+    printf("%s: ", __func__);
+    printf("%p\n", message);
+    // Освобождаем память, выделенную под строку
+    if (message != NULL) {
+        free(message);
+        message = NULL;
+    }
 }
